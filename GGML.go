dac394686398be51a830fb0bcf455e54145f603a
Anders Brander <anders@brander.dk>
2023-09-24 02:41:57 +0200

    Implement GGML.String().

--- GGML.go
+++ GGML.go
@@ -1,5 +1,9 @@
 package gguf
 
+import (
+	"fmt"
+)
+
 type GGML int
 
 const (
@@ -21,3 +25,44 @@ const (
 	GgmlInt16   GGML = 17
 	GgmlInt32   GGML = 18
 )
+
+func (g GGML) String() string {
+	switch g {
+	case GgmlFloat32:
+		return "float32"
+	case GgmlFloat16:
+		return "float16"
+	case GgmlQ4_0:
+		return "q4_0"
+	case GgmlQ4_1:
+		return "q4_1"
+	case GgmlQ5_0:
+		return "q5_0"
+	case GgmlQ5_1:
+		return "q5_1"
+	case GgmlQ8_0:
+		return "q8_0"
+	case GgmlQ8_1:
+		return "q8_1"
+	case GgmlQ2_K:
+		return "q2_k"
+	case GgmlQ3_K:
+		return "q3_k"
+	case GgmlQ4_K:
+		return "q4_k"
+	case GgmlQ5_K:
+		return "q5_k"
+	case GgmlQ6_K:
+		return "q6_k"
+	case GgmlQ8_K:
+		return "q8_k"
+	case GgmlInt8:
+		return "int8"
+	case GgmlInt16:
+		return "int16"
+	case GgmlInt32:
+		return "int32"
+	default:
+		return fmt.Sprintf("GGML(%d)", g)
+	}
+}
