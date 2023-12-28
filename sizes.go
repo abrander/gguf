2db7df36274f24e67eec58b7c78f5a24fe13ec5a
Anders Brander <anders@brander.dk>
2023-12-28 01:32:31 +0100

    Fix tensor size calculation for 16 and 32 bit types.

--- sizes.go
+++ sizes.go
@@ -15,8 +15,8 @@ var sizes = map[GGML]struct {
 	blocksize     uint64
 	valuesinblock uint64
 }{
-	GgmlFloat32: {blocksize: 4, valuesinblock: 4},
-	GgmlFloat16: {blocksize: 2, valuesinblock: 2},
+	GgmlFloat32: {blocksize: 4, valuesinblock: 1},
+	GgmlFloat16: {blocksize: 2, valuesinblock: 1},
 	GgmlQ4_0:    {blocksize: 2 + qK4_0/2, valuesinblock: qK4_0},
 	GgmlQ4_1:    {blocksize: 4 + qK4_1/2, valuesinblock: qK4_1},
 	GgmlQ5_0:    {blocksize: 2 + 4 + qK5_0/2, valuesinblock: qK5_0},
@@ -30,6 +30,6 @@ var sizes = map[GGML]struct {
 	GgmlQ6_K:    {blocksize: qK_K/2 + qK_K/4 + qK_K/16 + 2, valuesinblock: qK_K},
 	GgmlQ8_K:    {blocksize: 4 + qK_K + 2*qK_K/16, valuesinblock: qK_K},
 	GgmlInt8:    {blocksize: 1, valuesinblock: 1},
-	GgmlInt16:   {blocksize: 2, valuesinblock: 2},
-	GgmlInt32:   {blocksize: 4, valuesinblock: 4},
+	GgmlInt16:   {blocksize: 2, valuesinblock: 1},
+	GgmlInt32:   {blocksize: 4, valuesinblock: 1},
 }
